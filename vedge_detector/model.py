35c2e5a4e87467362483c0a7e123dc8f664abe3f
Alejandro © <acocac@gmail.com>
2022-02-09 21:36:22 +0000

    add provider option and subset bands

--- vedge_detector/model.py
+++ vedge_detector/model.py
@@ -5,6 +5,7 @@ import tempfile
 import os
 import skimage.transform
 from PIL import Image
+import xarray as xr
 
 class vedge_detector:
     def __init__(self, model_json: str="https://zenodo.org/record/6023284/files/Model_VedgeDetector.json?download=1",
@@ -36,9 +37,32 @@ class vedge_detector:
         loaded_model.load_weights(os.path.join(self.temp_dir.name,'model.hdf5'))
         return loaded_model
 
-    def predict(self, image: np.ndarray) -> np.ndarray:
+    def predict(self, image: np.ndarray, provider: str = "planet") -> np.ndarray:
 
-        resized = skimage.transform.resize(image, (480, 480, 3))
+        image_xr = xr.DataArray(image, dims=['y', 'x', 'band'],
+                                      coords={'y': np.arange(image.shape[0]),
+                                              'x': np.arange(image.shape[1]),
+                                              'band': np.arange(image.shape[2])})
+
+        # subset RGB bands
+        channels_provider = {
+            'sentinel': ["cirrus", "blue", "red", "green", "NIR"],
+            'planet': ["blue", "red", "green", "NIR"],
+            'rgb': ["blue", "red", "green"]
+        }
+
+        image_channels = channels_provider[provider]
+        target_bands = ["red", "green", "NIR"]
+
+        image_all = image_xr.assign_coords(band_id=('band', image_channels))
+        image_all = image_all.set_index(band="band_id")
+
+        if len(image_all.band) > 3:
+            image_target = image_all.sel(band=target_bands)
+        else:
+            image_target = image_all
+
+        resized = skimage.transform.resize(image_target, (480, 480, 3))
         resized = np.expand_dims(resized, axis=0)
 
         pred = self.pretrained_model.predict(resized)
